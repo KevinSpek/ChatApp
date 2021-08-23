2c4fc1013468aafa0aca3767c2b2bf3ca0cca8cc
kevin <kvspek@gmail.com>
2021-08-23 19:56:52 +0300

    added user class

---
+++ lib/model/user.dart
@@ -0,0 +1,14 @@
+class User {
+  final String uid;
+  final String tag;
+  String nickname;
+
+  String imgPath = '';
+  DateTime lastUpdated = DateTime.now();
+
+  User({
+    required this.uid,
+    required this.tag,
+    required this.nickname,
+  });
+}
--- lib/view/home_views/home_screen_desktop.dart
+++ lib/view/home_views/home_screen_desktop.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:groupidy/colors.dart';
 import 'package:groupidy/controller/home_controller.dart';
-import 'package:groupidy/view/components/message.dart';
 
 import '../../typography.dart';
 import '../../utils.dart';
