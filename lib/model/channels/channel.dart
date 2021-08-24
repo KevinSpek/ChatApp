8b7bde6e773153f7dd77bf777317fcd67ade9dab
kevin <kvspek@gmail.com>
2021-08-24 11:02:27 +0300

    minor bug fix

--- lib/model/channels/channel.dart
+++ lib/model/channels/channel.dart
@@ -1,16 +1,15 @@
 class Channel {
   final String pid;
   final String name;
-  final String imgPath;
-  final String? iconText;
+
+  String iconText = '';
+  String imgPath = '';
   DateTime lastUpdated = DateTime.now();
   List<String>? uidsAllowed;
   List<String>? uidsAllowedToWrite;
 
   Channel({
-    this.pid,
-    this.name,
-    this.imgPath,
-    this.iconText,
+    required this.pid,
+    required this.name,
   });
 }
