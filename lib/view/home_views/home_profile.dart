f5932022756720898140fc227cc0ece56ee94b84
ntnlbar <ntnlbar@gmail.com>
2021-09-07 19:21:36 +0300

    Added home profile widget

---
+++ lib/view/home_views/home_profile.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:groupidy/colors.dart';
+import 'package:groupidy/model/user.dart';
+import 'package:groupidy/typography.dart';
+
+class HomeProfile extends StatefulWidget {
+  const HomeProfile({Key? key}) : super(key: key);
+
+  @override
+  _HomeProfileState createState() => _HomeProfileState();
+}
+
+class _HomeProfileState extends State<HomeProfile> {
+  User _currentUser = User(uid: "uid", tag: "As2U", nickname: "ntnlbar");
+
+  void _showToast(BuildContext context, String message) {
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
+  void _handleCopyTag(BuildContext context) {
+    Clipboard.setData(
+        ClipboardData(text: _currentUser.nickname + "#" + _currentUser.tag));
+    _showToast(context, "User tag copied to clipboard.");
+  }
+
+  void _handleTagChange(BuildContext context) {
+    // Handle tag change async
+    _showToast(context, "Tag changed succesfully.");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: kSecondaryBackground,
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 160,
+                  height: 160,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: kWhiteSecondary),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16),
+                            child: Text(
+                              _currentUser.nickname + "#" + _currentUser.tag,
+                              style: kSubTitle.copyWith(color: kWhite),
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () => _handleCopyTag(context),
+                            icon: Icon(
+                              Icons.copy,
+                              color: kWhite,
+                            ),
+                          ),
+                          IconButton(
+                              onPressed: () => _handleTagChange(context),
+                              icon: Icon(
+                                Icons.refresh,
+                                color: kWhite,
+                              ))
+                        ],
+                      ),
+                      Text(
+                        "Share your tag with friends so they can contact you!",
+                        style: kBodySmall.copyWith(color: kWhiteSecondary),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
