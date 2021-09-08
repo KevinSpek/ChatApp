c88823cb6bb038bddda2bc51ae0064462af49569
ntnlbar <ntnlbar@gmail.com>
2021-09-08 11:51:23 +0300

    Added circle image widget

---
+++ lib/view/components/circle_image.dart
@@ -0,0 +1,39 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:groupidy/colors.dart';
+
+class CircleImage extends StatelessWidget {
+  const CircleImage(
+      {Key? key,
+      this.imagePath = "",
+      this.size = 80,
+      this.placeholderPath = "images/user_placeholder.png",
+      this.placeholderBackgroundColor = kSecondaryBubble,})
+      : super(key: key);
+
+  final String imagePath;
+  final double size;
+  final String placeholderPath;
+  final Color placeholderBackgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(size / 2),
+        child: CachedNetworkImage(
+          imageUrl: imagePath,
+          placeholder: (context, url) => CircularProgressIndicator(),
+          errorWidget: (context, url, error) => Container(
+            color: placeholderBackgroundColor,
+            child: Image(
+              image: AssetImage(placeholderPath),
+              width: size,
+              height: size,
+            ),
+          ),
+          width: size,
+          height: size,
+          fit: BoxFit.cover,
+        ));
+  }
+}
