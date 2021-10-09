abdd8ce3729867e3a5a3049ef2fecbdcbf6e7ee4
kevin <kvspek@gmail.com>
2021-10-09 19:35:10 +0300

    Added firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:groupidy/colors.dart';
@@ -16,28 +17,49 @@ import 'package:groupidy/view/splash_view/splash_layout.dart';
 import 'package:groupidy/view/welcome_view/welcome_layout.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   setUrlStrategy(PathUrlStrategy());
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Groupidy',
-      initialRoute: '/home',
-      routes: {
-        '/': (context) => WelcomeLayout(),
-        '/join': (context) => JoinLayout(),
-        '/login': (context) => LoginLayout(),
-        '/splash': (context) => SplashLayout(),
-        '/home': (context) => HomeLayout(),
-        '/group': (context) => GroupLayout(),
-        '/test': (context) => WelcomeLayout(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          print(snapshot.error);
+          return MaterialApp(home: Text("Problem with app"));
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Groupidy',
+            initialRoute: '/home',
+            routes: {
+              '/': (context) => WelcomeLayout(),
+              '/join': (context) => JoinLayout(),
+              '/login': (context) => LoginLayout(),
+              '/splash': (context) => SplashLayout(),
+              '/home': (context) => HomeLayout(),
+              '/group': (context) => GroupLayout(),
+              '/test': (context) => WelcomeLayout(),
+            },
+          );
+        }
+
+        return SplashLayout();
       },
     );
   }
